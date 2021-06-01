ed8744d3a1234d6d99b058eb9f66c6176b49be36
Maks <sat2151@ukr.net>
2021-06-01 23:23:14 +0300

    lesson 12 completed

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-let =numberOfFilms=prompt+("Сколько фильмов ви уже поспотрели")
-console.log(numberOfFilims)
-console.log(typeof(numberOfFilims))
+const numberOfFilms=+prompt("Сколько фильмов ви уже поспотрели",'');
+
+
+const pernalMovieDB={
+    count:{},
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+const firstAnswer=prompt("Один из последних просмоттрених фильмов",'');
+const secondAnswer=prompt("На сколько оцените его?",'');
+const thirdAnswer=prompt("Один из последних просмоттрених фильмов",'');
+const fourhAnswer=prompt("На сколько оцените его?",'');
+
+
+pernalMovieDB.movies[firstAnswer]=secondAnswer; 
+pernalMovieDB.movies[thirdAnswer]=fourhAnswer;
+console.log(pernalMovieDB.movies)
