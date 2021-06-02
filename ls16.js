8818fe0843b1a6fef7d4edc1813823cbe92d9ec8
Maks <sat2151@ukr.net>
2021-06-02 21:00:43 +0300

    lesson 16 completed

---
+++ ls16.js
@@ -0,0 +1,21 @@
+// function showFirstMessage(x,y){
+//     console.log(x*y)
+// }
+// showFirstMessage(2,5)
+
+// console.log('хуй'*8)
+
+
+console.log(false=='') //true
+console.log(false==[]) //true
+console.log(false=={}) //false
+console.log(''==0) //true
+console.log(''==[]) //true
+console.log(''=={})//false
+console.log(0==[])//true
+console.log(0=={})//false
+console.log(0==null)//false
+
+console.log()
+
+
